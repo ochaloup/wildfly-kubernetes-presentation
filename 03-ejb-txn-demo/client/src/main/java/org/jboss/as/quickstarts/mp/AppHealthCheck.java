bc23b1e78e401f98323e41edc6f3624e129b295c
Ondra Chaloupka <ochaloup@redhat.com>
2020-01-26 10:26:15 +0100

    necessary

--- 03-ejb-txn-demo/client/src/main/java/org/jboss/as/quickstarts/mp/AppHealthCheck.java
+++ 03-ejb-txn-demo/client/src/main/java/org/jboss/as/quickstarts/mp/AppHealthCheck.java
@@ -1,15 +1,12 @@
 package org.jboss.as.quickstarts.mp;
 
-import org.eclipse.microprofile.health.Health;
-import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 
-import javax.enterprise.context.ApplicationScoped;
-
-@Health
-@ApplicationScoped
-public class AppHealthCheck implements HealthCheck {
-    @Override
+// @Liveness
+// @ApplicationScoped
+public class AppHealthCheck
+        // implements HealthCheck
+{
     public HealthCheckResponse call() {
         return HealthCheckResponse.up("OK");
     }
